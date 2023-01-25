27c98792263f5ff9fde88bd6ccc8159a65cedb77
hefreen <hefree@ya.ru>
2023-01-25 14:59:57 +0400

    Flake8 redact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
-
+from graphic_arts.start_game_banner import run_screensaver
+"""Super RPG Game"""
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -78,7 +79,11 @@ def choice_char_class() -> str:
     return char_class
 
 
+#if __name__ == '__main__':
+
 def main() -> str:
+    """Функция приветствия."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,3 +93,5 @@ def main() -> str:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+
+main()
